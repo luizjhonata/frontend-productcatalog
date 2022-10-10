dd2841407bf7822bb18117522dc45651a6b9d0e4
Jhonata Luiz <jhonataluiz@hotmail.com>
2022-10-10 11:26:06 -0300

    updated product and login page

--- src/components/ProductPage/index.tsx
+++ src/components/ProductPage/index.tsx
@@ -1,34 +1,59 @@
 import React from 'react'
 import './styles.css'
+import search from '../../assets/search.svg';
+import edit from '../../assets/edit.svg';
+import details from '../../assets/details.svg';
 
 function ProductPage() {
     return (
         <div className='product-card'>
             <h2 className='product-title'>Produtos</h2>
+            <div className='product-search-container'>
+                <h2></h2>
+                <input type="text" placeholder="Digite o código do produto" />
+                <img src={search} alt="" />
+
+            </div>
             <div>
                 <table className='product-table'>
                     <thead>
                         <tr>
-                            <th>Cod</th>
+                            <th className='show580'>Id</th>
+                            <th className='show580'>Cod</th>
                             <th>Description</th>
+                            <th className='show850'>Weight</th>
                             <th>Price</th>
+                            <th>Details</th>
+                            <th>Edit</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td>C11JGSDN80</td>
+                            <td className='show580'>1</td>
+                            <td className='show580'>C11JGSDN80</td>
                             <td>CURVA 11º BOLSA DN80</td>
+                            <td className='show850'>16</td>
                             <td>R$ 200.02</td>
+                            <td><img src={details} alt="Ver Detalhes" /></td>
+                            <td><img src={edit} alt="Editar Produto" /></td>
                         </tr>
                         <tr>
-                            <td>C45JGSDN80</td>
-                            <td>CURVA 45º BOLSA DN80</td>
-                            <td>R$ 267.02</td>
+                            <td className='show580'>1</td>
+                            <td className='show580'>C11JGSDN80</td>
+                            <td>CURVA 11º BOLSA DN80</td>
+                            <td className='show850'>16</td>
+                            <td>R$ 200.02</td>
+                            <td><img src={details} alt="Ver Detalhes" /></td>
+                            <td><img src={edit} alt="Editar Produto" /></td>
                         </tr>
                         <tr>
-                            <td>EBFDN100</td>
-                            <td>EXTREMIDADE BOLSA FLANGE DN100</td>
-                            <td>R$ 299.02</td>
+                            <td className='show580'>1</td>
+                            <td className='show580'>C11JGSDN80</td>
+                            <td>CURVA 11º BOLSA DN80</td>
+                            <td className='show850'>16</td>
+                            <td>R$ 200.02</td>
+                            <td><img src={details} alt="Ver Detalhes" /></td>
+                            <td><img src={edit} alt="Editar Produto" /></td>
                         </tr>
                     </tbody>
                 </table>
