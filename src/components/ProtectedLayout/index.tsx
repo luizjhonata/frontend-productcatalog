ce93e7a67a73ad74547ff4580dc5e1bf6443a50b
Jhonata Luiz <jhonataluiz@hotmail.com>
2023-03-14 17:35:10 -0300

    fix: problems in recovery user data and send user to the correctly page, when new page is opened

--- src/components/ProtectedLayout/index.tsx
+++ src/components/ProtectedLayout/index.tsx
@@ -1,12 +1,12 @@
-import { useAuth } from "../../contexts/AuthProvider/useAuth"
-import { Navigate } from 'react-router-dom';
+import { useAuth } from "../../contexts/AuthProvider/useAuth";
+import LoginPage from "../LoginPage";
 
 
 //Quando a regra do ProtectedLayout estiver correta vamos receber um children - {children : JSX.Element} é a criação de uma interface inline
 export const ProtectedLayout = ({ children }: { children: JSX.Element }) => {
     const auth = useAuth()
     if (!auth.username) {
-        return <Navigate to="/login" />;
+        return <LoginPage />;
     }
-    return <>{children}</>
+    return children
 };
